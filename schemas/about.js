35585795c208ea086573f30d143b4b8ef60e9d5c
Umma Ahimsha <umma.abadilla@codemi.co.id>
2021-11-09 01:47:55 +0700

    fix block content

--- schemas/about.js
+++ schemas/about.js
@@ -1,32 +1,51 @@
 export default {
-    name: 'about',
-    title: 'About Page',
-    type: 'document',
-    fields: [
-      {
-        name: 'title',
-        title: 'Title',
-        type: 'string'
+  name: 'about',
+  title: 'About Page',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'subtitle',
+      title: 'Subtitle',
+      type: 'text',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
       },
-      {
-        name: 'subtitle',
-        title: 'Subtitle',
-        type: 'text'
-      },
-      {
-        name: 'image',
-        title: 'Image',
-        type: 'image',
-        options: {
-          hotspot: true,
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'array',
+      of: [
+        { type: 'block' },
+        {
+          type: 'image',
+          fields: [
+            {
+              type: 'text',
+              name: 'alt',
+              title: 'Alternative text',
+              description: `Some of your visitors cannot see images, 
+                be they blind, color-blind, low-sighted; 
+                alternative text is of great help for those 
+                people that can rely on it to have a good idea of 
+                what\'s on your page.`,
+              options: {
+                isHighlighted: true,
+              },
+            },
+          ],
         },
-      },
-      {
-        name: 'description',
-        title: 'Description',
-        type: "array",
-        of: [{ type: "block" }],
-      }, 
-    ],
-  }
-  
+      ],
+    },
+  ],
+};
